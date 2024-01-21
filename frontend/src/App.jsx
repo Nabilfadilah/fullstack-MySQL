7b12b0378b1ad90b011274f9e3b1ed0c7e59e0ad
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-01-21 09:28:34 +0700

    add router project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
+import UserList from "./components/UserList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <p>mencoba memperbaiki</p>
-        <h1>Aaaaa kasian Aaaaaa</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="add" element={<AddUser />} />
+          <Route path="edit/:id" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
