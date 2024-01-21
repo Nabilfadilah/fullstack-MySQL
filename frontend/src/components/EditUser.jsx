4b56d986e38862d5154ffb515f74e468a3b53cb0
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-01-21 09:28:08 +0700

    add file edit user

---
+++ frontend/src/components/EditUser.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(`http://localhost:5000/users/${id}`, {
+        name,
+        email,
+        gender,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUserById = async () => {
+    const response = await axios.get(`http://localhost:5000/users/${id}`);
+    setName(response.data.name);
+    setEmail(response.data.email);
+    setGender(response.data.gender);
+  };
+
+  const backUser = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <form onSubmit={updateUser}>
+          {/* name */}
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+          </div>
+
+          {/* email */}
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+
+          {/* gender */}
+          <div className="field">
+            <label className="label">Gender</label>
+            <div className="control">
+              <div className="select is-fullwidth">
+                <select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Famale">Famale</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <button type="submit" className="button is-success">
+              Update
+            </button>
+
+            <button
+              type="button"
+              className="button is-danger"
+              onClick={backUser}
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
